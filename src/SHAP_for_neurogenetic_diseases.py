58d7acb9491c3eb85980d430a4459df097d0ea20
Sasha Petrenko <sap625@umsystem.edu>
2024-01-12 15:23:42 -0600

    Fix map and applymap usage in SHAP

--- src/SHAP_for_neurogenetic_diseases.py
+++ src/SHAP_for_neurogenetic_diseases.py
@@ -57,7 +57,12 @@ def main(display=True):
 
     # Convert feature values to 0 or 1
     # features = features.applymap(lambda x: 1 if x > 0 else 0)
-    features = features.map(lambda x: 1 if x > 0 else 0)
+    # features = features.map(lambda x: 1 if x > 0 else 0)
+    try:
+        # pandas API changed at 2.1.0
+        features = features.map(lambda x: 1 if x > 0 else 0)
+    except AttributeError:
+        features = features.applymap(lambda x: 1 if x > 0 else 0)
 
     # Concatenate 'features' and 'labels' along the columns axis
     df_neurogenetic = pd.concat([labels, features], axis=1)
